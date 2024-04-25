4ecaf6a3faf0615ca14a291d4ba59ba32c95887b
Akash Sharma <aakash6263264@gmail.com>
2024-04-25 23:24:19 +0530

    deploying locally for testing purpose

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-export const server = "http://localhost:4000/api";
+export const server =
+  "https://3149-2401-4900-1c7b-4512-7194-2a13-3d5b-1653.ngrok-free.app/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
