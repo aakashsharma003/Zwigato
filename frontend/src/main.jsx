0019b3f72fb3eaf42eb8dc4c6aa2f3ac84aa384d
Akash Sharma <aakash6263264@gmail.com>
2024-04-26 08:39:43 +0530

    updating API after deploying backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-export const server = "http://localhost:4000/api";
+export const server = "https://zwigato-jax0.onrender.com/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
