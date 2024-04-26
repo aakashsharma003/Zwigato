bc106683d4c687bfdefdba97112fc60c53369032
Akash Sharma <aakash6263264@gmail.com>
2024-04-26 12:33:06 +0530

    deploying again

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import paymentRoute from "./routes/paymentRoute.js";
 import cors from "cors";
 import { paymentVerification } from "./controllers/paymentController.js";
 
-config({ path: "./config/config.env" });
+config({ path: "./config/.env" });
 export const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,4 +13,3 @@ app.use("/api", paymentRoute);
 app.get("/api/getkey", (req, res) => {
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
 });
-// app.get("/api/paymentsuccess", paymentVerification);
