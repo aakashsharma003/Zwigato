b68a0e536670c66607f8ead4562b692697a9f76f
Akash Sharma <aakash6263264@gmail.com>
2024-04-26 08:36:22 +0530

    fixing issues related to .env configeration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-export const server =
-  "https://3149-2401-4900-1c7b-4512-7194-2a13-3d5b-1653.ngrok-free.app/api";
+export const server = "http://localhost:4000/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
