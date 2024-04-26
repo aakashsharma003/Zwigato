888e2773b51531cce6e6c76351befb438b8d38b0
Akash Sharma <aakash6263264@gmail.com>
2024-04-26 12:22:30 +0530

    fixing Payment Status

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // https://zwigato-jax0.onrender.com
 // http://localhost:4000
-export const server = "http://localhost:4000/api";
+export const server = "https://zwigato-jax0.onrender.com/api";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
